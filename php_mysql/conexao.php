9755a5be033238d8555eb1007e88094e14d2c361
Damon Dudek <damondudek@outlook.com>
2020-06-29 20:24:56 -0300

    CRUD, conexão e MER

---
+++ php_mysql/conexao.php
@@ -0,0 +1,48 @@
+<?php
+  function conectar_banco_de_dados() {
+    $servidorOO = "localhost";
+    $usuarioOO = "root";
+    $senhaOO = "";
+    $bando_de_dadosOO = "php";
+
+    $conexaoOO = new mysqli($servidorOO, $usuarioOO, $senhaOO, $bando_de_dadosOO);
+
+    if ($conexaoOO->connect_error) {
+        die("Ops! Conexão falhou :( " . $conexaoOO->connect_error);
+    }
+  }
+
+  // conexão procedural
+
+  echo "Conexão procedural --- inicio <br>";
+
+  $servidor = "localhost";
+  $usuario = "root";
+  $senha = "";
+  $bando_de_dados = "php";
+  
+  $conexao = mysqli_connect($servidor, $usuario, $senha, $bando_de_dados);
+  
+  if (!$conexao) {
+    die("Ops! Conexão falhou :( " . mysqli_connect_error());
+  }
+
+  echo "Conectado com sucesso! <br>";
+  echo "Conexão procedural --- fim <br><br>";
+
+  echo "Conexão OO - Orientada a objetos --- inicio <br>";
+  $servidorOO = "localhost";
+  $usuarioOO = "root";
+  $senhaOO = "";
+  $bando_de_dadosOO = "php";
+
+  $conexaoOO = new mysqli($servidorOO, $usuarioOO, $senhaOO, $bando_de_dadosOO);
+
+  if ($conexaoOO->connect_error) {
+      die("Ops! Conexão falhou :( " . $conexaoOO->connect_error);
+  }
+
+  echo "Conectado com sucesso! <br>";
+  echo "Conexão OO - Orientada a objetos --- fim";
+
+?>
